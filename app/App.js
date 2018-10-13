f4b9aa6c7e5d6257ca1825eb4bacd32c77391214
Ikamari <Sempai320941>
2018-10-13 10:28:40 +0300

    Initial commit

---
+++ app/App.js
@@ -0,0 +1,13 @@
+// React
+import React, { Component } from 'react';
+// Styles
+import styles               from './App.css'
+
+export default class App extends Component {
+    render() {
+        return(
+            <div className={styles.app}>
+            </div>
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'main': './app/index.js'
+    },
+    watch: true,
+    mode: "development",
+    output: {
+        path: __dirname + '/public',
+        filename: './js/app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'env', 'stage-2']
+                },
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                    modules: true,
+                    localIdentName: '[name]__[local]___[hash:base64:5]'
+                }
+            },
+            {
+                test: /\.(ttf|otf|gif|svg|jpg|png)$/,
+                loader: "file-loader",
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: './resources/',
+                    publicPath: './resources/'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css')
+    ]
+};
